42f3af5e80c45a51ac8657be76ad69e663f7f889
tamkevindo97 <66348479+tamkevindo97@users.noreply.github.com>
2020-06-12 13:39:43 -0700

    Added notes and an extra test case

--- palindrome.py
+++ palindrome.py
@@ -1,23 +1,26 @@
 import string
 
+# Returns the reverse of a string
 def reverse(string):
     rev = ""
-    for i in range(1,len(string)+1):
+    for i in range(1, len(string)+1):
         if len(rev) == len(string):
             print(rev)
         else:
             rev = rev + string[-i]
     return rev
 
+# Modifies string to remove spaces and punctuation before comparing with reverse
 def palindrome(s):
     mod1 = s.replace(" ", "")
     mod2 = mod1.lower()
     mod3 = mod2.translate(str.maketrans('', '', string.punctuation))
     if reverse(mod3) == mod3:
-        print("'", s , "'", "is a palindrome")
+        print("'", s, "'", "is a palindrome!")
     else:
-        print("'", s , "'", "is not a palindrome")
+        print("'", s, "'", "is not a palindrome.")
 
-
-palindrome("madam i'm adam")
-palindrome("madam i'm kevin")
+# Test cases
+palindrome("Madam i'm Adam")
+palindrome("Madam i'm Kevin")
+palindrome("Live not on evil")
