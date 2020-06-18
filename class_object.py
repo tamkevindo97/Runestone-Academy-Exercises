110b5df34e7dbbf5a0093448850cd351f1714b5f
tamkevindo97 <66348479+tamkevindo97@users.noreply.github.com>
2020-06-18 11:16:10 -0700

    Updated class_object.py with more intuitive string conversion

--- class_object.py
+++ class_object.py
@@ -10,11 +10,11 @@ class Point:
         return self.y
 
     def distfromorigin(self):
-        return "Distance = " + str(((self.x**2) + (self.y**2))**(1/2))
+        return "Distance from Origin = " + str(((self.x**2) + (self.y**2))**(1/2))
 
     def string(self):
-        return "x = " + str(self.x) + " y = " + str(self.y)
+        return "Point = ({0},{1})".format(self.x, self.y)
 
 a = Point(3, 4)
-print(a.distfromorigin())
 print(a.string())
+print(a.distfromorigin())
